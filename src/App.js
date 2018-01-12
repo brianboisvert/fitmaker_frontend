3459034896be72f96dddef4678a624cc66ef3d50
github username <github email address>
2018-01-12 11:21:08 -0500

    added form for login

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import WorkoutList from './containers/workoutList';
 import WorkoutForm from './containers/WorkoutForm'
+import Login from './components/Login'
 
 class App extends React.Component {
   render() {
     return (
       <div>
+        <Login />
         <WorkoutForm />
         <WorkoutList />
       </div>
